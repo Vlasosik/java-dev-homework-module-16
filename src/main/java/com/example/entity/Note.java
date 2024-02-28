0efc4368f8df79e8d1efb5e91c705720b84d3d96
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-02-28 21:38:10 +0200

    added the note entity

---
+++ src/main/java/com/example/entity/Note.java
@@ -0,0 +1,26 @@
+package com.example.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "note")
+@Getter
+@Setter
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "context")
+    private String context;
+}
